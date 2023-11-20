eb6849c111871cf21589e3b4fb00db5d0a452ad8
Andy Jones <jones.andrew.e@gmail.com>
2023-11-19 19:03:48 -0700

    Removed some auth providers

--- template/src/data/auth-providers.js
+++ template/src/data/auth-providers.js
@@ -5,8 +5,6 @@ import { Facebook as FacebookIcon } from "mdi-material-ui";
 import { Github as GitHubIcon } from "mdi-material-ui";
 import { Google as GoogleIcon } from "mdi-material-ui";
 import { Microsoft as MicrosoftIcon } from "mdi-material-ui";
-import { Twitter as TwitterIcon } from "mdi-material-ui";
-import { Yahoo as YahooIcon } from "mdi-material-ui";
 
 const authProviders = [
   {
